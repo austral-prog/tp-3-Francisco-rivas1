69c24eabcf1df1bf344a177f637bc338d1a0ee80
Francisco-rivas1 <frivas2@mail.austral.edu.ar>
2025-04-07 10:40:44 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,6 @@
 def check_vowels():
+    nombre = input()
+    name=nombre.lower
     if "a" in name:
         print("Contiene a: True")
     else:
@@ -19,8 +21,6 @@ def check_vowels():
         print("Contiene u: True")
     else:
         print("Contiene u: False")
-nombre = input()
-name=nombre.lower
 check_vowels()
     # Código a implementar utilizando input.
 
