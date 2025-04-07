9c0e5fdacac61cad98abdb903a4921a3c0aa07cd
Francisco-rivas1 <frivas2@mail.austral.edu.ar>
2025-04-07 10:25:55 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,9 @@
 def slice_advanced():
+    print(text[4::2])
+text="Hello, World!"
+slice_advanced()
+text="12345678910"
+slice_advanced()
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
